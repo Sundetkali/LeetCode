1303860104075da0a62228be748c33fc3e7639f4
Сүндетқали Анарбай <anarbay_sundetkali@live.kaznu.kz>
2023-01-12 21:25:39 +0600

    [add] 58. Length of Last Word

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LeetCode;
+
+Solution solution = new();
+Console.WriteLine(solution.LengthOfLastWord("Hello horld"));
+;
--- LeetCode/Solution.cs
+++ LeetCode/Solution.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LeetCode
+﻿namespace LeetCode
 {
-    internal class Solution
+    public class Solution
     {
+        public int LengthOfLastWord(string s)
+        {
+            int k = 0;
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (s[i] == ' ')
+                {
+                    break;
+                }
+                else
+                {
+                    k++;
+                }
+            }
+            return k;
+        }
+
+        public bool IsPalindrom(string s)
+        {
+            for (int i = 0, j = s.Length - 1; i < j; i++, j--)
+            {
+
+
+                if (s[i] == s[j])
+                    continue;
+                return false;
+
+
+            }
+            return true;
+        }
     }
+
 }
